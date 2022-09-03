e96077739f856f3d7e869f505664144080d3221d
Marcio Emmanuel <100731829+M-Emmanuel-G@users.noreply.github.com>
2022-09-02 22:45:40 -0300

    dsfkhds

--- pro-auto/src/pages/screenClient/index.js
+++ pro-auto/src/pages/screenClient/index.js
@@ -1,85 +1,126 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import HeaderContainer from '../../components/header';
 import { ContainerBase } from '../../style';
 import { ButtonCamp, InfoClientCamp, ScreenClientCamp, TittleClient, InputEdit } from './style';
 import { useNavigate } from 'react-router-dom'
 import useRequestData from '../../hooks/useRequestData';
 import { useEffect } from 'react';
-import Swal from 'sweetalert2';
+import { ClientContext } from '../../context/context';
+import ImgSend from "../../img/send.png"
+import ImgBack from "../../img/back.png"
+import ImgEdit from "../../img/edit.png"
 
 function ScreenClient() {
-    const [renderClient, setRenderClient] = useState([])
     const navigate = useNavigate()
     const [ data ] = useRequestData("clients.json")
     const [ inputEdit, setInputEdit ] = useState("")
+    const context = useContext(ClientContext)
+    const [ iRua, setIRua ] = useState("")
+    const [ iNumero, setINumero ] = useState("")
+    const [ iBairro, setIBairro ] = useState("")
+    const [ iCidade, setICidade ] = useState("")
+    const [ iEstado, setIEstado ] = useState("")
 
     useEffect(()=>{
         const client = data.filter(client=> client.cpf === localStorage.getItem("cpfCliente") && client.placa === localStorage.getItem("placaCliente"));
-        setRenderClient(client)
+        context.setRenderClient(client)
     },[data])
 
-    const showEdit= ()=>{
-        document.getElementById("editCamp").style.display = "flex"
-
-        
-    }
-    
     const sendEdit = (ev)=>{
         ev.preventDefault()
-        const edited = renderClient.map((c)=>{
-            console.log(c);
+        setInputEdit(`${iRua},${iNumero},${iBairro},${iCidade},${iEstado} `)
+        document.getElementById("editCamp").style.opacity = 0
+        const edited = context.renderClient.map((c)=>{
             return {...c, address:inputEdit}
            })
-           setRenderClient(edited)
-           document.getElementById("editCamp").style.display = "none"
+           context.setRenderClient(edited)
     }
+
+    const cancelEdit = ()=>{
+        document.getElementById("editCamp").style.opacity = 0
+    }
+
+    const edit = (ev)=>{
+        ev.preventDefault()
+        document.getElementById("editCamp").style.opacity = 1
+    }
+
  return (
    <div>
     <ContainerBase>
         <HeaderContainer/>
         <ScreenClientCamp>
             <InfoClientCamp>
-                {renderClient.map((c, key)=>{
+                {context.renderClient.map((c, key)=>{
                     
                     return(
                         <>
-                            <TittleClient> <h1>Informações</h1></TittleClient>
+                            <TittleClient>
+                                <img onClick={()=>{navigate("/login")}} src={ImgBack}/>
+                                <h1>Informações</h1>
+                            </TittleClient>
                             <table key={key}>
                                 <thead>
                                     <tr>
                                         <td>Cliente:</td>
                                         <td>{c.name}</td>
+                                        <td><img onClick={()=>{alert("Entre em contato conosco para atualizar esta informação.")}} src={ImgEdit}/></td>
                                     </tr>
                                     <tr>
                                         <td>CPF:</td>
                                         <td>{c.cpf}</td>
+                                        <td><img onClick={()=>{alert("Entre em contato conosco para atualizar esta informação.")}} src={ImgEdit}/></td>
                                     </tr>
                                     <tr>
                                         <td>Placa:</td>
                                         <td>{c.placa}</td>
+                                        <td><img onClick={()=>{alert("Entre em contato conosco para atualizar esta informação.")}} src={ImgEdit}/></td>
                                     </tr>
                                     <tr>
                                         <td>Endereço:</td>
                                         <td>{c.address}</td>
+                                        <td><img onClick={edit} src={ImgEdit}/></td>
                                     </tr>
                                     <tr>
                                         <td>Telefone:</td>
                                         <td>{c.phone}</td>
+                                        <td><img onClick={()=>{alert("Entre em contato conosco para atualizar esta informação.")}} src={ImgEdit}/></td>
                                     </tr>
                                 </thead>
                             </table>
-                <ButtonCamp>
-                    <button onClick={()=>{navigate("/login")}}>voltar</button>
-                    <button onClick={showEdit}>Editar</button>
-                </ButtonCamp>
                 <InputEdit id='editCamp'>
-                    <input
-                        value={inputEdit}
-                        onChange={(ev)=>{setInputEdit(ev.target.value)}}
-                        placeholder='rua/avenida,numero, bairro, cidade, estado '
-                    />
                     <form onSubmit={sendEdit}>
-                        <button>send</button>
+                        <div>
+                            <input
+                                value={iRua}
+                                onChange={(ev)=>{setIRua(ev.target.value)}}
+                                placeholder='rua/avenida'
+                            />
+                            <input
+                                value={iNumero}
+                                onChange={(ev)=>{setINumero(ev.target.value)}}
+                                placeholder='numero'
+                            />
+                            <input
+                                value={iBairro}
+                                onChange={(ev)=>{setIBairro(ev.target.value)}}
+                                placeholder='bairro'
+                            />
+                            <input
+                                value={iCidade}
+                                onChange={(ev)=>{setICidade(ev.target.value)}}
+                                placeholder='cidade'
+                            />
+                            <input
+                                value={iEstado}
+                                onChange={(ev)=>{setIEstado(ev.target.value)}}
+                                placeholder='estado'
+                            />
+                        </div>
+                        <div>
+                            <button>send</button>
+                            <button onClick={cancelEdit} type='button'>Cancelar</button>
+                        </div>
                     </form>
                 </InputEdit>
                         </>
--- pro-auto/src/style.js
+++ pro-auto/src/style.js
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const ContainerBase = styled.div`
     width: 100vw;
     height: 100vh;
-    background: yellow;
+    background: #f4f4f2;
 `
