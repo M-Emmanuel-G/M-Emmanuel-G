90cef3cdaf067a3c972128a926cce87e71959348
Marcio Emmanuel <100731829+M-Emmanuel-G@users.noreply.github.com>
2024-04-08 13:37:37 -0300

    Delete portifolio directory

--- portifolio/src/App.js
+++
@@ -1,9 +0,0 @@
-import Selector from "./components/SelectorPage";
-
-function App() {
-  return (
-    <Selector/>
-  );
-}
-
-export default App;
--- portifolio/src/Components/layout/style.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components"
-
-export const LayoutContainer = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background: linear-gradient(36deg, rgba(12,13,13,1) 26%, rgba(54,52,52,1) 76%);
-`
